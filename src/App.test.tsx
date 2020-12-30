1f1b177bcb002df7d42147f7585f5ae7f57bf9bf
donghak <steadyzest@gmail.com>
2020-12-30 15:33:23 +0900

    테스트 문구 변경

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen } from "@testing-library/react";
+import App from "./App";
 
-test('renders learn react link', () => {
+test("메인페이지 텍스트 포함여부 확인", () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/메인페이지/i);
   expect(linkElement).toBeInTheDocument();
 });
