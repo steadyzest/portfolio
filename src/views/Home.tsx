6a542aabd51da6a56efb91bb9075773fe22e8e17
donghak <steadyzest@gmail.com>
2021-01-02 23:38:13 +0900

    그리드로 변경

--- src/views/Home.tsx
+++ src/views/Home.tsx
@@ -1,45 +1,85 @@
 import React from "react";
-import { useHistory } from "react-router-dom";
-import { Container, Grid, Box, Button } from "@material-ui/core";
+// import { useHistory } from "react-router-dom";
+import { Grid, Box } from "@material-ui/core";
+
+const BoxWhiteColor = {
+  color: "#FFFFFF",
+};
+const BoxRedPinkColor = {
+  color: "#FF407D",
+};
 
 const Home = () => {
-  let history = useHistory();
+  // let history = useHistory();
 
-  const MovePage = (path: string) => {
-    history.push(`${path}`);
-  };
+  // const MovePage = (path: string) => {
+  //   history.push(`${path}`);
+  // };
 
   return (
-    <Container>
-      <Box mt={3} pt={3} style={{ color: "#33F1D8" }}>
-        <Box>Developer</Box>
-        <Box>KWON DONG HAK</Box>
-      </Box>
+    <Grid container>
+      {/* 개발자명 */}
+      <Grid container>
+        <Grid item xs={2} />
+        <Grid item xs={8}>
+          <Box
+            mt={3}
+            pt={3}
+            fontSize="1.3rem"
+            fontWeight="bold"
+            color="#33F1D8"
+          >
+            <Box>Developer</Box>
+            <Box>KWON DONG HAK</Box>
+          </Box>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
 
-      <Box mt={3} pt={3}>
-        <Box
-          style={{
-            fontSize: "5rem",
-            WebkitTextStroke: "1px white",
-            WebkitTextFillColor: "rgba(255, 255, 255, 0.02)",
-            opacity: 0.5,
-          }}
-        >
-          Any Desing,
-        </Box>
-        <Box style={{ color: "#FFFFFF" }}>I can make it!</Box>
-      </Box>
+      {/* 문구1 */}
+      <Grid container>
+        <Grid item xs={3} />
+        <Grid item xs>
+          <Box mt={4} pt={4} fontSize="4rem" fontWeight="bold">
+            <Box
+              style={{
+                WebkitTextStroke: "1px white",
+                WebkitTextFillColor: "rgba(255, 255, 255, 0.02)",
+                opacity: 0.5,
+              }}
+            >
+              Any Desing,
+            </Box>
+            <Box style={BoxWhiteColor}>I can make it!</Box>
+          </Box>
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
 
-      <Box mt={3} pt={3}>
-        <Box style={{ color: "#ffffff" }}>어떤 디자인이든,</Box>
-        <Box style={{ color: "#FFFFFF" }}>제가 만들어 드릴게요!</Box>
-      </Box>
+      {/* 문구2 */}
+      <Grid container>
+        <Grid item xs={3} />
+        <Grid item xs>
+          <Box mt={3} pt={3} fontSize="1.5rem">
+            <Box style={BoxWhiteColor}>어떤 디자인이든,</Box>
+            <Box style={BoxWhiteColor}>제가 만들어 드릴게요!</Box>
+          </Box>
+        </Grid>
+        <Grid item xs={3} />
+      </Grid>
 
-      <Box mt={3} pt={3} display="flex" flexDirection="row">
-        <Box style={{ color: "#FF407D" }}>want to hire me?</Box>
-        <Box style={{ color: "#FF407D" }}>ㅡㅡㅡㅡ</Box>
-        <Box style={{ color: "#FF407D" }}>yes!</Box>
-      </Box>
+      {/* 마지막 문구 */}
+      <Grid container>
+        <Grid item xs={8} />
+        <Grid item xs>
+          <Box mt={3} pt={3} display="flex" flexDirection="row">
+            <Box style={BoxRedPinkColor}>want to hire me?</Box>
+            <Box style={BoxRedPinkColor}>ㅡㅡㅡㅡ</Box>
+            <Box style={BoxRedPinkColor}>yes!</Box>
+          </Box>
+        </Grid>
+        <Grid item xs={2} />
+      </Grid>
 
       {/* <Grid>
         <Box>
@@ -55,7 +95,7 @@ const Home = () => {
           <Button onClick={() => MovePage("contact")}>Contact</Button>
         </Box>
       </Grid> */}
-    </Container>
+    </Grid>
   );
 };
 
